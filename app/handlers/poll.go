7b3e3cdbe32f307e31874af82cd836399afb79a0
ThePianoDentist <jbknight07@gmail.com>
2021-04-09 17:40:35 +0100

    hack status codes

--- app/handlers/poll.go
+++ app/handlers/poll.go
@@ -10,8 +10,8 @@ import (
 
 func Poll(appCtx *app_context.AppContext, w http.ResponseWriter, r *http.Request) {
   if appCtx.Status != "CONFIRMED"{
-    utils.SuccessResp(appCtx.Lgr, w, 200, map[string]int{"n": 0})
+    utils.SuccessResp(appCtx.Lgr, w, 444, map[string]int{"n": 0})
     return
   }
-  utils.SuccessResp(appCtx.Lgr, w, 200, map[string]int{"n": len(appCtx.Drinkers)})
+  utils.SuccessResp(appCtx.Lgr, w, 200 + len(appCtx.Drinkers), map[string]int{"n": len(appCtx.Drinkers)})
 }
